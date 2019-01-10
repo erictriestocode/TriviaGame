f4cb4b504549ebcfb93e710f3ac6f95c51255425
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-01-09 21:29:58 -0600

    added questions, basic variables, got CSS working, some new fonts

---
+++ javascript/app.js
@@ -0,0 +1,46 @@
+// JS file for the trivia game assignment(easy)
+
+// **** Some mental notes for myself below as I go along to chart my line of thinking ****
+//
+// if userInput = question.correctAnswer, make correct status true, correctTotal++
+
+var timer = 120000;
+var totalCorrect = 0;
+var questionArray = [questionOne,questionTwo,questionThree];
+var userChoice;
+
+
+// questions
+
+var questionOne = {
+    text: "In what year did the US declare Independence?",
+    answer1: "1620",
+    answer2: "1776",
+    answer3: "1812",
+    answer4: "1782",
+
+};
+
+var questionTwo = {
+    text: "How many players are on a baseball field during play?",
+    answer1: "8",
+    answer2: "9",
+    answer3: "10",
+    answer4: "11",
+
+};
+
+var questionThree = {
+    text: "What kind of person shall not be honored on a postage stamp?",
+    answer1: "A King or Queen",
+    answer2: "A Sports Star",
+    answer3: "A President",
+    answer4: "A living person",
+};
+
+
+// Functions
+
+function checkcorrect() {
+    if (userChoice === )
+};
